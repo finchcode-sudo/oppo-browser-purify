39911d417e706aed15130f8588df0ca3d7a16b31
finchcode-sudo <poqq26566@gmail.com>
2026-09-11 21:11:03 +0800

    Update PurifyEntry.java

--- app/src/main/java/com/heytap/purify/PurifyEntry.java
+++ app/src/main/java/com/heytap/purify/PurifyEntry.java
@@ -6,7 +6,6 @@ import com.heytap.purify.hooks.BottomTabPurifyHook;
 import com.heytap.purify.hooks.DownloadManagerPurifyHook;
 import com.heytap.purify.hooks.MyProfilePurifyHook;
 import com.heytap.purify.hooks.PureSearchPurifyHook;
-import com.heytap.purify.hooks.SpeedUpHook;
 import com.heytap.purify.hooks.HookPrefs;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
@@ -15,17 +14,17 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class PurifyEntry implements IXposedHookLoadPackage {
 
-    public static final String BROWSER_PACKAGE = "com.heytap.browser";
+public static final String BROWSER_PACKAGE = "com.heytap.browser";
     public static final String TAG = "OPPO浏览器净化";
 
-    @Override
+@Override
     public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
 
-        if (!BROWSER_PACKAGE.equals(lpparam.packageName)) {
+if (!BROWSER_PACKAGE.equals(lpparam.packageName)) {
             return;
         }
 
-        if (!BROWSER_PACKAGE.equals(lpparam.processName)) {
+if (!BROWSER_PACKAGE.equals(lpparam.processName)) {
             return;
         }
         HookPrefs.init();
@@ -35,10 +34,9 @@ public class PurifyEntry implements IXposedHookLoadPackage {
         install("AiVisionPurifyHook", () -> AiVisionPurifyHook.install(lpparam.classLoader));
         install("AdBlockPurifyHook", () -> AdBlockPurifyHook.install(lpparam.classLoader));
         install("PureSearchPurifyHook", () -> PureSearchPurifyHook.install(lpparam.classLoader));
-        install("SpeedUpHook", () -> SpeedUpHook.install(lpparam.classLoader));
     }
 
-    private static void install(String name, Runnable hook) {
+private static void install(String name, Runnable hook) {
         try {
             hook.run();
         } catch (Throwable t) {
